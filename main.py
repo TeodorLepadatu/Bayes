ad5b72b15c1066c4ba747a58edc9209a83cc286e
TEO <teodor.lepadatu@yahoo.com>
2024-11-08 19:56:25 +0200

    data proper

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def read_csv(file):
     while line:
         line = line.strip().split(";")
         line.pop()
+        line[2]=line[3]
+        line.pop()
         data.append(line)
         line=f.readline()
     return data
 
 if __name__ == '__main__':
     data = read_csv("Reddit_Combi.csv")
-    print(data)
+    print(data[0][0])
