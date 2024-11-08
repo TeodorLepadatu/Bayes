1bba92bf973c48d070784d3b758a79a34e10f23b
ROXANA-GEORGIANAASAV <asaveiroxana7@gmail.com>
2024-11-08 21:13:24 +0200

    Calcul probabilitati

--- main.py
+++ main.py
@@ -22,23 +22,41 @@ def add_to_dict(dict, *lists):
                 dict[word] = 1
 
 def parse_data(data, dict_pos, dict_neg):
+    global no_pos, no_neg, no_total
     for i in range (len(data)):
-        words_title  = [x.strip(",:“”!/\*?'. ()[]_’‘-꒰⌯͒•̩̩̩́'ᴗ•̩̩̩̀⌯͒꒱↓~&—+{}").lower() for x in data[i][0].split()]
-        words_text = [x.strip(",:“”!/\*?'. ()[]’_‘-꒰⌯͒•̩̩̩́'ᴗ•̩̩̩̀⌯͒꒱↓~&—+{}").lower() for x in data[i][1].split()]
+        words_title  = [x.strip(",*:“”!/?'. ()[]_’‘-꒰⌯͒•̩̩̩́'ᴗ•̩̩̩̀⌯͒꒱↓~&—+{\\}").lower() for x in data[i][0].split()]
+        words_text = [x.strip(",:*“”!/?'. ()[]’_‘-꒰⌯͒•̩̩̩́'ᴗ•̩̩̩̀⌯͒꒱↓~&—+{\\}").lower() for x in data[i][1].split()]
         try:
             if int(data[i][2]) == 0:
                 add_to_dict(dict_neg, words_title, words_text)
+                no_neg += 1
             else:
                 add_to_dict(dict_pos, words_title, words_text)
+                no_pos += 1
+            no_total += 1
         except:
             pass
+
+
+def prob_word(dict):
+    no_words = sum(dict.values())
+    for key in dict.keys():
+        dict[key] = round(dict[key] / no_words, 10)
 
 if __name__ == '__main__':
     data = read_csv("Reddit_Combi.csv")
     # print(data[0])
     dict_pos = {}
     dict_neg = {}
+    no_pos = no_neg = no_total = 0
     parse_data(data, dict_pos, dict_neg)
+    prob_word(dict_pos)
+    prob_word(dict_neg)
+    # print(no_pos / no_total)
+   # print(no_neg)
+   #  print(no_total)
+    # print(dict_neg)
+    # print(dict_neg)
     #print(len(dict_pos))
     #print(len(dict_neg))
 
